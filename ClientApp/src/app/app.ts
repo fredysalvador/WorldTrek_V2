d50a2f321bafadada7d6d57a8b1eb28dd56db2cc
fredysalvador <fredy2003f@gmail.com>
2025-09-18 16:57:50 -0600

    Adaptacion de Angular ya redirige de C# a Angular en el dashboard

--- ClientApp/src/app/app.ts
+++ ClientApp/src/app/app.ts
@@ -1,14 +1,10 @@
-// src/app/app.ts
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   template: `<router-outlet></router-outlet>`,
-  providers: [provideRouter(appRoutes)],
 })
 export class App {}
